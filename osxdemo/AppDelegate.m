de5b94553f59a87a8e34d21f618292197073301f
Open Fibers <openfibers@gmail.com>
2013-11-04 10:15:51 +0800

    Initial Commit

---
+++ osxdemo/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  osxdemo
+//
+//  Created by openthread on 11/4/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ osxdemo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  osxdemo
+//
+//  Created by openthread on 11/4/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
